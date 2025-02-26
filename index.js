e88c559c524ce4eb409feb5c558a565f4049acf5
abinashnimz <abinash443steam@gmail.com>
2025-02-26 23:55:57 +0530

    added best api structure for data source

--- index.js
+++ index.js
@@ -14,44 +14,129 @@ const app = express();
 app.use(express.json());
 
 const API_KEY = "AIzaSyANoeQcBUQYhF_3Zx0XT2DlYqr4FI-Shhk";
-const LIVE_VIDEO_ID = "9ElsF45ejUg";
+const LIVE_VIDEO_ID = "gaQx5-FevXw";
 
 let chatMessages = [];
 
-let hashTag = {
-    "falcons":0,
-    "141":0,
-    "asi8":0,
-    "leo":0,
-    "ste":0,
-    "t2k":0,
-    "a1":0,
-    "4t":0,
-    "star":0,
-    "drs":0,
-    "asl":0,
-    "horaa":0,
-    "4mv":0,
-    "7e":0,
-    "ihc":0,
-    "r3g":0,
-    "east":0,
-    "voltrux":0,
-    "uzm":0,
-    "haitdami":0,
-    "riley":0,
-    "rulz":0,
-    "sinister":0,
-    "falak":0,
-    "fury":0,
-    "delta":0,
-    "chari":0,
-    "sleepy":0,
-    "dok":0,
-    "goku":0,
-    "alex":0,
-    "blade":0,
-};
+// let hashTag = {
+//     "falcons":0,
+//     "141":0,
+//     "asi8":0,
+//     "leo":0,
+//     "ste":0,
+//     "t2k":0,
+//     "a1":0,
+//     "4t":0,
+//     "star":0,
+//     "drs":0,
+//     "asl":0,
+//     "horaa":0,
+//     "4mv":0,
+//     "7e":0,
+//     "ihc":0,
+//     "r3g":0,
+//     "east":0,
+//     "voltrux":0,
+//     "uzm":0,
+//     "haitdami":0,
+//     "riley":0,
+//     "rulz":0,
+//     "sinister":0,
+//     "falak":0,
+//     "fury":0,
+//     "delta":0,
+//     "chari":0,
+//     "sleepy":0,
+//     "dok":0,
+//     "goku":0,
+//     "alex":0,
+//     "blade":0,
+// };
+
+let team_data = [
+    {
+        "team_name":"falcons",
+        "score":0,
+        "team_logo":"",
+    },
+    {
+        "team_name":"141",
+        "score":0,
+        "team_logo":"",
+    },
+    {
+        "team_name":"asi8",
+        "score":0,
+        "team_logo":"",
+    },
+    {
+        "team_name":"leo",
+        "score":0,
+        "team_logo":"",
+    },
+    {
+        "team_name":"ste",
+        "score":0,
+        "team_logo":"",
+    },
+    {
+        "team_name":"t2k",
+        "score":0,
+        "team_logo":"",
+    },
+    {
+        "team_name":"a1",
+        "score":0,
+        "team_logo":"",
+    },
+    {
+        "team_name":"4t",
+        "score":0,
+        "team_logo":"",
+    },
+    {
+        "team_name":"star",
+        "score":0,
+        "team_logo":"",
+    },
+    {
+        "team_name":"drs",
+        "score":0,
+        "team_logo":"",
+    },
+    {
+        "team_name":"asl",
+        "score":0,
+        "team_logo":"",
+    },
+    {
+        "team_name":"horaa",
+        "score":0,
+        "team_logo":"",
+    },
+    {
+        "team_name":"4mv",
+        "score":0,
+        "team_logo":"",
+    },
+    {
+        "team_name":"7e",
+        "score":0,
+        "team_logo":"",
+    },
+    {
+        "team_name":"ihc",
+        "score":0,
+        "team_logo":"",
+    },
+    {
+        "team_name":"r3g",
+        "score":0,
+        "team_logo":"",
+    },
+];
+
+
 
 // Function to get Live Chat ID
 async function getLiveChatId() {
@@ -77,16 +162,25 @@ async function fetchLiveChatMessages(liveChatId) {
                 message: item.snippet.displayMessage,
                 timestamp: item.snippet.publishedAt
             };
-            // console.log(messageData);
 
             // Avoid duplicate messages
             if (!chatMessages.some(msg => msg.timestamp === messageData.timestamp)) {
                 chatMessages.push(messageData);
                 console.log("New message:", messageData);
-                let newMsg = messageData.message;
-                for(let key in hashTag){
-                    if(newMsg.includes(`#${key}`)){
-                        hashTag[key]+=1;
+                let newMsg = messageData.message.toLowerCase();
+                // for(let key in hashTag){
+                //     if(newMsg.includes(`#${key}`)){
+                //         hashTag[key]+=1;
+                //     }
+                // }
+                // for(let item of team_hashtags){
+                //     if(newMsg.includes(`#${item.team_name}`)){
+                //         item.score+=1;
+                //     }
+                // }
+                for(let i=0; i<team_data.length; i++){
+                    if(newMsg.includes(`#${team_data[i].team_name}`)){
+                        team_data[i].score+=1;
                     }
                 }
             }
@@ -96,11 +190,9 @@ async function fetchLiveChatMessages(liveChatId) {
     }
 }
 
-console.log(chatMessages)
-
 // API Route to get stored messages
 app.get("/chats", (req, res) => {
-    res.json(hashTag);
+    res.json(team_data);
 });
 
 // Start fetching messages every 3 seconds
@@ -108,7 +200,7 @@ async function startFetching() {
     const liveChatId = await getLiveChatId();
     if (liveChatId) {
         console.log("Live Chat ID:", liveChatId);
-        setInterval(() => fetchLiveChatMessages(liveChatId), 3000);
+        setInterval(() => fetchLiveChatMessages(liveChatId), 5000);
     } else {
         console.log("No active live chat found.");
     }
